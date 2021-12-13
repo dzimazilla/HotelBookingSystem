cd4ea506c6fc50b76a0034e27bd12de2ee4caffb
Linlynne <55032044+Linlynne@users.noreply.github.com>
2021-12-13 16:56:43 -0500

    add

---
+++ src/main/java/com/springboot/hotelbookingsystem/domain/Address.java
@@ -0,0 +1,90 @@
+package com.example.hotelbookingsystem.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address implements Serializable{
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100)
+    private String address;
+    @Column(length = 100)
+    private String city;
+    @Column(length = 100)
+    private String province;
+    @Column(length = 100)
+    private String postcode;
+    @Column(length = 100)
+    private String country;
+    @OneToOne(mappedBy = "address")
+    private User user;
+    
+    
+    
+	public Address() {
+
+
+	}
+	public Address(String address, String city, String province, String postcode, String country) {
+		super();
+		this.address = address;
+		this.city = city;
+		this.province = province;
+		this.postcode = postcode;
+		this.country = country;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
