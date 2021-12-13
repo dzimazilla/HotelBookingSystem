62ad1f607c79349c5c3583c2a08701b19ba47987
Linlynne <55032044+Linlynne@users.noreply.github.com>
2021-12-13 16:53:35 -0500

    add

---
+++ src/main/java/com/springboot/hotelbookingsystem/domain/Role.java
@@ -0,0 +1,54 @@
+package com.example.hotelbookingsystem.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role implements Serializable{
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100)
+    private String name;
+    @OneToOne(mappedBy = "role")
+    private User user;
+    
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public Role() {
+
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
