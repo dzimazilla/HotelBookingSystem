a47913aa757d42f36562e642fa7731ae9e954775
Linlynne <55032044+Linlynne@users.noreply.github.com>
2021-12-13 16:52:48 -0500

    add

---
+++ src/main/java/com/springboot/hotelbookingsystem/domain/User.java
@@ -0,0 +1,132 @@
+package com.example.hotelbookingsystem.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import java.io.Serializable;
+@Entity
+@Table(name = "user")
+public class User implements Serializable{
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 50)
+    @NotNull(message = "user name can't be null")
+    private String username;
+
+    @Column(length = 500)
+    @NotNull(message = "password can't be null")
+    private String password;
+    
+	@Column(length = 100)
+    @NotNull(message = "email can't be null")
+    @Email
+    private String email;
+
+	@Column(length = 100)
+    @NotNull(message = "mobile can't be null")
+    private String mobile;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL)
+    private Set<Reservation> reservations = new HashSet<Reservation>();
+
+	public User(@NotNull(message = "user name can't be null") String username,
+			@NotNull(message = "password can't be null") String password,
+			@NotNull(message = "email can't be null") @Email String email,
+			@NotNull(message = "mobile can't be null") String mobile, Address address, Role role
+			) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.mobile = mobile;
+		this.address = address;
+		this.role = role;
+	}
+
+	public User() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+}
