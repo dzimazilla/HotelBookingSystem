c6ec717204db922ee4ce5d64b378c6bda645ad74
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2021-12-14 16:14:29 -0500

    rearranged structure of files and packages part 1

---
+++ src/main/java/com/springboot/hotelbookingsystem/entities/Address.java
@@ -0,0 +1,96 @@
+package com.springboot.hotelbookingsystem.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "address")
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100)
+    private String address;
+    @Column(length = 100)
+    private String city;
+    @Column(length = 100)
+    private String province;
+    @Column(length = 100)
+    private String postcode;
+    @Column(length = 100)
+    private String country;
+    @OneToOne(mappedBy = "address")
+    private User user;
+
+
+    public Address() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public Address(String address, String city, String province, String postcode, String country) {
+        super();
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postcode = postcode;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
--- src/main/java/com/springboot/hotelbookingsystem/models/RegisterModel.java
+++ src/main/java/com/springboot/hotelbookingsystem/models/RegisterModel.java
@@ -1,67 +1,85 @@
-package com.example.hotelbookingsystem.model;
+package com.springboot.hotelbookingsystem.models;
 
 public class RegisterModel {
-	private String username;
-	private String password;
-	private String email;
-	private String mobile;
-	private String address;
-	private String city;
-	private String province;
-	private String postcode;
-	private String country;
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getMobile() {
-		return mobile;
-	}
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getProvince() {
-		return province;
-	}
-	public void setProvince(String province) {
-		this.province = province;
-	}
-	public String getPostcode() {
-		return postcode;
-	}
-	public void setPostcode(String postcode) {
-		this.postcode = postcode;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    private String username;
+    private String password;
+    private String email;
+    private String mobile;
+    private String address;
+    private String city;
+    private String province;
+    private String postcode;
+    private String country;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
