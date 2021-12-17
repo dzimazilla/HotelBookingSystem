0efee481adbe5f2e538304ec7bb610762a3398b1
Linlynne <55032044+Linlynne@users.noreply.github.com>
2021-12-16 23:18:50 -0500

    Update CustomUserDetails.java

--- src/main/java/com/springboot/hotelbookingsystem/security/CustomUserDetails.java
+++ src/main/java/com/springboot/hotelbookingsystem/security/CustomUserDetails.java
@@ -7,56 +7,54 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 
-public class CustomUserDetails implements UserDetails {
-    private User user;
-
+public class CustomUserDetails  implements UserDetails{
+	private User user;
+    
     public CustomUserDetails(User user) {
         this.user = user;
     }
-
+ 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
-
+ 
     @Override
     public String getPassword() {
         return user.getPassword();
     }
-
+ 
     @Override
     public String getUsername() {
         return user.getUsername();
     }
-
+ 
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
+ 
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
+ 
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
+ 
     @Override
     public boolean isEnabled() {
         return true;
     }
-
+    
     public String getEmail() {
         return user.getEmail();
     }
-
     public User getUser() {
         return user;
     }
-
     public String getRoleName() {
         return user.getRole().getName();
     }
