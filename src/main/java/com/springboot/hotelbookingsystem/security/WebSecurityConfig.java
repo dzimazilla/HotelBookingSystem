f825e8783df7aab3bfc9c6e2675b26410be7d7cf
Linlynne <55032044+Linlynne@users.noreply.github.com>
2021-12-16 23:20:54 -0500

    Update WebSecurityConfig.java

--- src/main/java/com/springboot/hotelbookingsystem/security/WebSecurityConfig.java
+++ src/main/java/com/springboot/hotelbookingsystem/security/WebSecurityConfig.java
@@ -13,18 +13,19 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
 
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService();
     }
-
+    
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -32,37 +33,49 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
     }
-
+   
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/h2-console/*");
+        return registrationBean;
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
+    	http.httpBasic().disable();
+    	//http.csrf().disable();
+    	//http.headers().frameOptions().disable();
         http.authorizeRequests()
-                .antMatchers("/admin/user").authenticated()
-                .antMatchers("/admin/room").authenticated()
-                .antMatchers("/admin/reservation").authenticated()
-                .antMatchers("/placeorder").authenticated()
-                .antMatchers("/").permitAll()
-                .antMatchers("/index").permitAll()
-                .antMatchers("/register").permitAll()
-                .and()
-                .formLogin()
-                .usernameParameter("username")
-                .loginPage("/login")
+            .antMatchers("/admin/user").authenticated()
+            .antMatchers("/admin/room").authenticated()
+            .antMatchers("/admin/reservation").authenticated()
+            .antMatchers("/admin/*/*").authenticated()
+            .antMatchers("/placeorder").authenticated()
+        	.antMatchers("/").permitAll()
+        	.antMatchers("/h2-console").permitAll()
+            .antMatchers("/index").permitAll()
+            .antMatchers("/register").permitAll()
+            .and()
+            .formLogin()
+            	.usernameParameter("username")
+            	.loginPage("/login")
+            	.failureUrl("/login-error")
                 .defaultSuccessUrl("/index")
                 .permitAll()
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .and()
-                .exceptionHandling()
-                .accessDeniedPage("/403");
+            .and()
+            .logout()
+	            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))            
+	            .logoutSuccessUrl("/login")
+	            .invalidateHttpSession(true)       
+	            .deleteCookies("JSESSIONID")
+	            .and()
+              .exceptionHandling()
+                  .accessDeniedPage("/403");
     }
 }
+
